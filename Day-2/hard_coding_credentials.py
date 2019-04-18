c8acccf27053125f15efcf5d6a0d95e69772fefe
AutomationWithScripting <49341609+AutomationWithScripting@users.noreply.github.com>
2019-04-18 13:13:25 +0530

    Updated the code

--- Day-2/hard_coding_credentials.py
+++ Day-2/hard_coding_credentials.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 import boto3
-root_session=boto3.session.Session(aws_access_key_id="AKIAITOXLWMUUVVCFMPA",aws_secret_access_key="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")
+root_session=boto3.session.Session(aws_access_key_id="XXXXXXXXXX",aws_secret_access_key="XXXXXXXXX")
 ec2_con=root_session.resource('ec2',"us-east-1")
 print dir(ec2_con)
